41b4fe073a58b191dcbc4d3d2b25efb85867e3e2
Ksenyacola <fl_n@bk.ru>
2023-11-21 17:53:54 +0300

    first iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCard(item) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = item.name;
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__image').alt = item.name;
 
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function() {
+        deleteCard(cardElement);
+    });
+
+    return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 // @todo: Вывести карточки на страницу
+function renderCards(cards) {
+    cards.forEach(item => {
+        const cardElement = createCard(item);
+        placesList.appendChild(cardElement);
+    });
+}
+
+renderCards(initialCards);
